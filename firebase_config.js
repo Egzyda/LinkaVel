567f937cba34feddd4ae85f0886121c2f34af99e
Egzyda <tokiyozaemon@gmail.com>
2026-01-07 14:04:32 +0900

    up

--- firebase_config.js
+++ firebase_config.js
@@ -5,13 +5,15 @@
 // 注意: Firebaseコンソール > Project Settings > General > Your apps から
 // CDN版の設定を取得して書き換えてください。
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCLO9xDiYRKqoBO6p8f9WEjMUsPJ_qym-Y",
+  authDomain: "egzyda.firebaseapp.com",
+  projectId: "egzyda",
+  storageBucket: "egzyda.firebasestorage.app",
+  messagingSenderId: "953734376474",
+  appId: "1:953734376474:web:7f295970f1797cb0b775a3",
+  measurementId: "G-0HM94115X8"
 };
 
 // Firebaseの初期化 (Compat版)
